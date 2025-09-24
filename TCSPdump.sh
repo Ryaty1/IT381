f508a2e927ec08d0989385cde67f4c1206a7c9fd
Ryaty1 <hengkimchhay1@gmail.com>
2025-09-23 16:21:14 -1000

    tcp done

--- TCSPdump.sh
+++ TCSPdump.sh
@@ -1,2 +1,18 @@
+#change permission on key file
+chmod 400 "/Users/kimchhayheng/Desktop/IT-381/IT381Keypair.pem"
 # Connect to AWS Server
-ssh -i "IT381Keypair.pem" ec2-user@ec2-54-226-42-219.compute-1.amazonaws.com
+ssh -i /Users/kimchhayheng/Desktop/IT-381/IT381Keypair.pem ec2-user@ec2-54-226-42-219.compute-1.amazonaws.com
+#check to see if tc[spdump is installed
+which tcpdump
+#Macos - Find the default interface
+route -n get default
+# Linux - Find the default interface
+ip route 
+# Use with ifconfig command with identified interface
+ifconfig ens5
+# TCPdump command to monitor port
+sudo tcpdump -i ens5 -s0 -v port 53
+# TCPdump to monitor udp
+sudo tcpdump -i ens5 udp
+# TCPdump to monitor tcp
+sudo tcpdump -i ens5 tcp
