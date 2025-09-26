c426e90e13538274d7cb6bc9ed0687591ffda8ec
Ryaty1 <hengkimchhay1@gmail.com>
2025-09-25 15:37:04 -1000

    save

--- TCSPdump.sh
+++ TCSPdump.sh
@@ -1,3 +1,6 @@
+route -n get default #Macos - Find the default interface, 
+
+
 #change permission on key file
 chmod 400 "/Users/kimchhayheng/Desktop/IT-381/IT381Keypair.pem"
 # Connect to AWS Server
@@ -16,3 +19,14 @@ sudo tcpdump -i ens5 -s0 -v port 53
 sudo tcpdump -i ens5 udp
 # TCPdump to monitor tcp
 sudo tcpdump -i ens5 tcp
+
+# tell tcpdump to stop after capturing 100 packets, use the -c option followed by the count.
+sudo tcpdump -i ens5 port 80 -c 100 -w output.pcap
+
+#The same option works when writting to a file:
+sudo tcpdump -i ens5 port 80 -c 100 -w output.pcap
+
+route -n get default #Macos - Find the default interface, this will change the command above(23) to sudo tcpdump -i <default_interface> port 80 -c 100 -w output.pcap e.g en0 for this mac.
+
+# TCPdump to monitor DHCP
+sudo tcpdump -i en0 port 67 or port 68 -e -n -vv
